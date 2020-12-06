930fe87026d600a5f70d8e553018a7480e85f9ca
jjanmo <jjanmo07@hanmail.net>
2020-12-07 00:12:36 +0900

    Update home style

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -7,7 +7,6 @@ class Home extends React.Component {
         return (
             <div className={styles.container}>
                 <h1 className={styles.title}>Mini React Apps</h1>
-                {/* <div> */}
                 <ul className={styles.listContainer}>
                     <li>
                         <Link to="/gugudan">구구단 퀴즈</Link>
@@ -22,7 +21,6 @@ class Home extends React.Component {
                         <Link to="/scissor-rock-paper">가위바위보 게임</Link>
                     </li>
                 </ul>
-                {/* </div> */}
             </div>
         );
     }
