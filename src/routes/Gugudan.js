3bdad662e36b7621ea5d3b30a2408c11c303e414
jjanmo <jjanmo07@hanmail.net>
2020-12-09 23:51:45 +0900

    Refactor from class component to functional component using hooks

--- src/routes/Gugudan.js
+++ src/routes/Gugudan.js
@@ -1,85 +1,81 @@
-import React from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import HomeButton from '../components/HomeButton';
 import RestartImg from '../assets/restart.png';
 import styles from '../styles/Gugudan.module.css';
 
-class Gugudan extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            number1: 0,
-            number2: 0,
-            message: '',
-            input: '',
-        };
-    }
+function Gugudan() {
+    const [firstNumber, setFirstNumber] = useState(1);
+    const [secondNumber, setSecondNumber] = useState(1);
+    const [message, setMessage] = useState('');
+    const [input, setInput] = useState('');
+    const numberInput = useRef();
 
-    componentDidMount() {
-        this.setNumber();
-    }
+    useEffect(() => {
+        setNewNumber();
+        numberInput.current.focus();
+    }, []);
 
-    setNumber() {
-        this.setState({
-            number1: Math.floor(Math.random() * 9 + 1),
-            number2: Math.floor(Math.random() * 9 + 1),
-        });
-    }
+    const setNewNumber = () => {
+        setFirstNumber(Math.floor(Math.random() * 9 + 1));
+        setSecondNumber(Math.floor(Math.random() * 9 + 1));
+    };
 
-    handleSubmit = (e) => {
+    const initializeInput = () => {
+        setInput('');
+        numberInput.current.focus();
+    };
+
+    const handleClick = () => {
+        setNewNumber();
+        numberInput.current.focus();
+    };
+
+    const handleSubmit = (e) => {
         e.preventDefault();
-        const { number1, number2, input } = this.state;
-        const submittedNumber = parseInt(input);
-        if (isNaN(submittedNumber)) {
-            this.setState({ message: '숫자만 입력할 수 있습니다. 😱', input: '' });
-        } else if (number1 * number2 === parseInt(input)) {
-            this.setNumber();
-            this.setState(() => {
-                return {
-                    message: `와우 🥳 맞췄습니다! (정답 : ${number1 * number2})`,
-                    input: '',
-                };
-            });
+        const correctAnswer = firstNumber * secondNumber;
+        const answer = parseInt(input, 10);
+        if (isNaN(answer)) {
+            setMessage('숫자만 입력할 수 있습니다. 😱');
+            initializeInput();
+        } else if (correctAnswer === parseInt(input, 10)) {
+            setNewNumber();
+            setMessage(`와우 🥳 맞췄습니다! (정답 : ${correctAnswer})`);
+            initializeInput();
         } else {
-            this.setState({ message: `꽝 🤯 틀렸습니다.`, input: '' });
+            setMessage('꽝 🤯 틀렸습니다.');
+            initializeInput();
         }
     };
 
-    handleChange = (e) => {
-        const { target } = e;
-        this.setState({ input: target.value });
+    const handleChange = (e) => {
+        const _input = e.target.value;
+        setInput(_input);
     };
 
-    handleClick = () => {
-        this.setNumber();
-    };
-
-    render() {
-        const { number1, number2, input, message } = this.state;
-        return (
-            <div className={styles.outerContainer}>
-                <h1 className={styles.title}>구구단을 외우자</h1>
-                <div className={styles.innerContainer}>
-                    <button className={styles.restartButton} onClick={this.handleClick}>
-                        <img className={styles.img} src={RestartImg} alt="restart" />
-                    </button>
-                    <div className={styles.question}>{`${number1} X ${number2} = ?`}</div>
-                    <form className={styles.form} onSubmit={this.handleSubmit}>
-                        <input
-                            className={styles.input}
-                            type="text"
-                            value={input}
-                            onChange={this.handleChange}
-                            autoFocus
-                            maxLength={3}
-                        />
-                        <input className={styles.submitButton} type="submit" value="제출" />
-                    </form>
-                    <div className={styles.message}>{message}</div>
-                </div>
-                <HomeButton />
+    return (
+        <div className={styles.outerContainer}>
+            <h1 className={styles.title}>구구단을 외우자</h1>
+            <div className={styles.innerContainer}>
+                <button className={styles.restartButton} onClick={handleClick}>
+                    <img className={styles.img} src={RestartImg} alt="restart" />
+                </button>
+                <div className={styles.question}>{`${firstNumber} X ${secondNumber} = ?`}</div>
+                <form className={styles.form} onSubmit={handleSubmit}>
+                    <input
+                        className={styles.input}
+                        type="text"
+                        value={input}
+                        onChange={handleChange}
+                        maxLength={3}
+                        ref={numberInput}
+                    />
+                    <input className={styles.submitButton} type="submit" value="제출" />
+                </form>
+                <div className={styles.message}>{message}</div>
             </div>
-        );
-    }
+            <HomeButton />
+        </div>
+    );
 }
 
 export default Gugudan;
